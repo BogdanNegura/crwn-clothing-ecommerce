eef15f3d0e472933d7790e16e2845756402e545e
Bogdan Negura <negura.bogdan.90@gmail.com>
2019-12-15 16:14:53 +0000

    style the content of directory passing properties throw component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -3,6 +3,10 @@ import Directory from "../../components/directory/directory.component";
 
 import "./homepage.styles.scss";
 
-const Homepage = () => <Directory />;
+const Homepage = () => (
+  <div className="homepage">
+    <Directory />;
+  </div>
+);
 
 export default Homepage;
